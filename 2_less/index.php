74f6ea5b3aa4978ec31a4a7fe2b3f4fbaa36221e
Evgeny Nikolaev <e.nikolaev@uplab.ru>
2021-02-11 14:13:50 +0300

    less

--- 2_less/index.php
+++ 2_less/index.php
@@ -1,6 +1,7 @@
 <? /*
 
 1) Знакомство с Less
+http://lesscss.org/
 
 
 Препроцессор - инструмент, преобразующий код из одного синтаксиса в другой.
@@ -60,46 +61,101 @@
 5) Переменные
     @headerFontSize: 2rem;
 
-6) Примеси
+6) Примеси (миксин, ф-ия)
+.bordered  (или #bordered  или bordered) - !желательно через точку
 
-		.bordered {
-		  border: 1px solid #000;
-		}
+		--- Пример1 ---
+			.bordered {
+			  border: 1px solid #000;
+			}
 
-		.inline-block( @valign:top ) {
-		  display: inline-block;
-		  vertical-align: @valign;
-		}
+			.inline-block( @valign:top ) {
+			  display: inline-block;
+			  vertical-align: @valign;
+			}
 
-		.img-container {
-		  .inline-block;
-		  width: 80px;
-		}
+			.img-container {
+			  .inline-block;
+			  width: 80px;
+			}
 
-		.box {
-		  .inline-block(middle);
-		}
+			.box {
+			  .inline-block(middle);
+			}
+
+
+	 --- пример2. конкатенация строк ---
+
+			@pathToIcon: 'img/tmp/';
+			@iconW: 400px;
+
+			.icon(@url) {
+			  display: inline-block;
+			  width: @iconW;
+			  background: url("@{pathToIcon}@{url}.png");
+			}
+
+			.header {
+			  .icon('logo');
+			}
+
+
+
+	 ---  пример3. выполнение миксина при условии  ---
+
+			.fontSize(@fs) when (isnumber(@fs)) {
+			  font-size: ~"@{fs}px"; // ~  !убирает кавычки после конкатенации
+			}
+
+			html {
+			  .fontSize(12);
+			}
+
+
+
+    ---  пример4. Циклы, сетка  ---
+
+			@tablet-width: 1170px;
+			@phone-width: 768px;
+
+			@columnCount: 12;
+
+			.grid(@name, @columnNumber:1) when (@columnNumber<=@columnCount) {
+			  .@{name}@{columnNumber} {
+			    width: (100%/@columnCount)*@columnNumber;
+			  }
+
+			  .grid (@name, @columnNumber+1); //цикл
+			}
+
+			//вызов
+			.grid(xs);
+			@media screen and (min-width: @phone-width) {
+			  .grid(sm);
+			}
+
+			@media screen and (min-width: @tablet-width) {
+			  .grid(lg);
+			}
+
 
 
 7) Ф-ии
+Полный перечень ф-ий здесь - http://lesscss.org/
 
 
+ Работа с изображениями
+    image-size(path_to_file) - возвращает размеры изображения (100px 100px)
+    image-width(path_to_file)
+    image-height(path_to_file) - возвращает отдельные значения высоты или ширины (в px)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ Работа с цветами
+    !!! Работают либо в 16-ричном формате, либо в HSL-формате
+		    lighten(color, percent) - увеличивает светлоту
+		    darken(color, percent) - уменьшает светлоту
+		    saturate(color, percent) - увеличивает насыщенность
+		    desaturate(color, percent) - уменьшает насыщенность
+		    mix(color, color, percent) - смешивает цвета в заданной пропорции
 
 
 
