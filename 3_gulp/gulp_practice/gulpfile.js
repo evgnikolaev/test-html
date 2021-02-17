29d3a42c795db96ea9b926ab79a7dd658fa4bc74
Evgeny Nikolaev <e.nikolaev@uplab.ru>
2021-02-17 20:21:46 +0300

    gulp

--- 3_gulp/gulp_practice/gulpfile.js
+++ 3_gulp/gulp_practice/gulpfile.js
@@ -1,28 +1,44 @@
 var gulp = require('gulp');
 var concatCss = require('gulp-concat-css');
+var includer = require('gulp-htmlincluder');
 var connect = require('gulp-connect');
+var livereload = require('gulp-livereload');
 
-gulp.task('css', function () {
+gulp.task('connect', function () {
+	connect.server({
+		root: 'build',
+		livereload: true
+	});
+});
+
+gulp.task('concatCss', function () {
 	return gulp.src('dev/**/*.css')     		    // gulp.src() -  путь относительно gulp.js, откуда взять файлы   (** - рекурсивно в папке)
 		.pipe(concatCss("bundle.css"))    	// .pipe() - задача, concatCss() - ф-ия плагина
-		.pipe(gulp.dest('build/'))      			//  gulp.dest() - путь, куда выложить результат
+		.pipe(gulp.dest('build/css'))      			//  gulp.dest() - путь, куда выложить результат
 		.pipe(connect.reload());
 });
 
-gulp.task('connect', function (done) {
-	connect.server({
-		root: 'build',
-		livereload: true,
-		port: 80,
+gulp.task('htmlIncluder', function () {
+	gulp.src('dev/**/*.html')
+		.pipe(includer())
+		.pipe(gulp.dest('build/'))
+		.pipe(connect.reload());
+});
+
+
+gulp.task('default', function () {
+
+	gulp.start('connect', 'concatCss', 'htmlIncluder');
+
+	gulp.watch(['dev/**/*.html'], function (event) {
+		gulp.start('htmlIncluder');
 	});
-	done()
+	gulp.watch(['dev/**/*.css'], function (event) {
+		gulp.start('concatCss');
+	});
 });
 
-gulp.task('watch', function () {
-	gulp.watch(['dev/**/*.css'], gulp.parallel('css'));
-});
 
-gulp.task('default', gulp.series('connect', 'css', 'watch'));
 
 
 
--- readme.md
+++ readme.md
@@ -4,21 +4,29 @@
 <br/>
 
 ```
-Doctype Html (1_theory)
+1) Doctype Html (1_theory)
 ```
 [youtube Doctype Html](https://www.youtube.com/watch?v=KOYSwrhANbY&list=PLir4Ol-qj7tXrfexvXgtmGFi9SOIMqa6y&index=1&ab_channel=DoctypeHtml)
 
 <br/><br/>
 
 ```
-Doctype Html (2_less)
+2) Doctype Html (2_less)
 ```
 [youtube Doctype Html](https://www.youtube.com/watch?v=6sq1_EgEsDI&list=PLir4Ol-qj7tVOn87gK_oMY52NOgwdNb9r&ab_channel=DoctypeHtml)
 
 <br/><br/>
 
 ```
-Doctype Html practice  (4_practice)
+3) Doctype gulp (3_gulp)
+```
+[gulp_for_small](https://www.youtube.com/watch?v=vW51JUVT66w&ab_channel=WebDesignMaster)
+
+[gulp_practice](https://www.youtube.com/watch?v=suVfAi04mN4&list=PLir4Ol-qj7tW_Zg6fxSUPM840R7OeWldh&ab_channel=DoctypeHtml)
+<br/><br/>
+
+```
+4) Doctype Html practice  (4_practice)
 ```
 
 [youtube Doctype Html practice](https://www.youtube.com/watch?v=ZTadiHsaQXY&list=PLir4Ol-qj7tVjQGPZh8LhJ87HUprfVeWU&ab_channel=DoctypeHtml)
