92ea6a28ea6253e18d1e9f885c14a4e6e3489041
Evgeny Nikolaev <e.nikolaev@uplab.ru>
2021-02-19 18:24:00 +0300

    html

---
+++ 4_practice/7_adaptive/gulpfile.js
@@ -0,0 +1,70 @@
+var gulp = require('gulp');
+var concatCss = require('gulp-concat-css');
+var includer = require('gulp-htmlincluder');
+var replace = require('gulp-html-replace');
+var connect = require('gulp-connect');
+var livereload = require('gulp-livereload');
+
+
+gulp.task('connect', function () {
+	connect.server({
+		root: 'build',
+		livereload: true
+	});
+});
+
+
+gulp.task('concatCss', function () {
+	return gulp.src('dev/**/*.css')     		    // gulp.src() -  путь относительно gulp.js, откуда взять файлы   (** - рекурсивно в папке)
+		.pipe(concatCss("bundle.css"))    	// .pipe() - задача, concatCss() - ф-ия плагина
+		.pipe(gulp.dest('build/css'))      			//  gulp.dest() - путь, куда выложить результат
+		.pipe(connect.reload());
+});
+
+gulp.task('htmlIncluder', function () {
+	gulp.src('dev/**/*.html')
+		.pipe(includer())
+		.pipe(replace({
+			css: 'css/bundle.css'
+		}))
+		.pipe(gulp.dest('build/'))
+		.pipe(connect.reload());
+});
+
+
+gulp.task('default', function () {
+
+	gulp.start('connect', 'concatCss', 'htmlIncluder');
+
+	gulp.watch(['dev/**/*.html'], function (event) {
+		gulp.start('htmlIncluder');
+	});
+	gulp.watch(['dev/**/*.css'], function (event) {
+		gulp.start('concatCss');
+	});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
