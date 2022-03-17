c40616cb53aa477c78ccccba1808ac01c6479840
Evgeny Nikolaev <e.nikolaev@uplab.ru>
2022-03-17 21:53:28 +0300

    swiper

---
+++ 9_tutorials/1_lazyload/index.php
@@ -0,0 +1,15 @@
+<?/*
+
+https://www.youtube.com/watch?v=IdUbsiQUxa4&list=PLM6XATa8CAG6h4q2Urs2_BDbKSKMquLiX&ab_channel=%D0%A4%D1%80%D0%B8%D0%BB%D0%B0%D0%BD%D1%81%D0%B5%D1%80%D0%BF%D0%BE%D0%B6%D0%B8%D0%B7%D0%BD%D0%B8-IT%D0%B8%D1%84%D1%80%D0%B8%D0%BB%D0%B0%D0%BD%D1%81
+
+
+Ленивая загрущка - загрузка когда доскроллили до блока.
+
+1) loading="lazy" - поддержка слабая очень
+2) при помощи js (дата атрибутом)
+
+
+
+
+
+*/?>
